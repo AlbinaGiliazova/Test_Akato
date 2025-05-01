76f7fa3b6b86a6a12bf2849f582f740449956179
Albina Giliazova <giliazova@mail.ru>
2025-05-01 14:10:36 +0300

    Add directories

--- akato/akato/settings.py
+++ akato/akato/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-a!y_)g$2)=+3p@t)*#4t(c13q3mb$3n_9tz@r$*bh!ym3-=$_*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'api',
+    'products',
+    'users',
 ]
 
 MIDDLEWARE = [

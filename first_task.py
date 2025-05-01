9e7698f1306cb1a23564bce230aa11e0d7cc2c17
Albina Giliazova <giliazova@mail.ru>
2025-05-01 13:08:36 +0300

    First task

---
+++ first_task.py
@@ -0,0 +1,23 @@
+"""1.Напишите программу, которая выводит n первых элементов последовательности 122333444455555… 
+(число повторяется столько раз, чему оно равно)."""
+
+
+def get_n_elements(n: int) -> None:
+    """Выводит n первых элементов последовательности 122333444455555…"""
+    current = 1
+    number = 0
+    quantity = 0
+    if n <= 0:
+        return None
+    while quantity < n:
+        number += 1
+        print(current, end='')
+        if number >= current:
+            current += 1
+            number = 0
+        quantity += 1
+
+
+if __name__ == '__main__':
+    n = int(input('Введите число элементов последовательности: '))
+    get_n_elements(n)
